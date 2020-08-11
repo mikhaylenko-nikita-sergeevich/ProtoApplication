e8cc7e50e432413e5db1ea006ee256b5c47ce879
Nikita Mikhaylenko <nikita.mihaylenko@tobox.com>
2020-08-11 03:54:14 +0300

    splash

--- app/src/main/java/com/proto/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/proto/ui/activity/SplashActivity.kt
@@ -1,13 +1,44 @@
 package com.proto.ui.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.proto.R
+import kotlinx.coroutines.*
+import kotlin.concurrent.thread
+
+/**
+ * Хорошая статья про стартовые экраны — https://habr.com/ru/post/345380/
+ */
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        //TODO:заставка и таймер для перехода
+        someDataLoadingSimulation()
     }
+
+    private fun someDataLoadingSimulation() {
+        processingWithCoroutines()
+    }
+
+    private fun processingWithCoroutines() {
+        GlobalScope.launch {
+            delay(3000L)
+            navigateToOnboardingScreen()
+        }
+    }
+
+    private fun processingWithThreads(){
+        thread {
+            Thread.sleep(3000L)
+            runOnUiThread { navigateToOnboardingScreen() }
+        }
+    }
+
+    private fun navigateToOnboardingScreen() {
+        startActivity(Intent(this, OnboardingActivity::class.java))
+    }
+
 }
---
+++ app/src/main/java/com/proto/ui/fragment/DetailInfoFragment.kt
@@ -0,0 +1,15 @@
+package com.proto.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.proto.R
+
+class DetailInfoFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_eee, container, false)
+}
